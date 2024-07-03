cbcb7b7344f60df036c064545a94633a0b78199c
aeternitas00 <j0hnsmith991133@gmail.com>
2024-07-03 17:46:06 +0900

    액티베이터블 카드 관련 테스트 / 액트 아젠다 관리 / 장소 이펙트 테스트

--- Source/ProjectDMSGame/EffectSet/Step/DMSSequenceStep_FT.cpp
+++ Source/ProjectDMSGame/EffectSet/Step/DMSSequenceStep_FT.cpp
@@ -1,44 +1,44 @@
 #include "EffectSet/Step/DMSSequenceStep_FT.h"
-#include "Library/DMSCoreFunctionLibrary.h"
-#include "GameModes/DMSGameStateBase.h"
-
-FGameplayTag UDMSSequenceStep_FT::GetPureStepTag_Implementation() const
-{
-	return FGameplayTag::RequestGameplayTag("Step.Arkham.FreeTrigger");
-}
-
-//TScriptInterface<IDMSEffectorInterface> UDMSSelector_FT::GetTriggerableEffectors()
+//#include "Library/DMSCoreFunctionLibrary.h"
+//#include "GameModes/DMSGameStateBase.h"
+//
+//FGameplayTag UDMSSequenceStep_FT::GetPureStepTag_Implementation() const
 //{
-//	return TScriptInterface<IDMSEffectorInterface>();
-//
-//	// additional condition check with owner step and find ft-able effectors.
-//
+//	return FGameplayTag::RequestGameplayTag("Step.Arkham.FreeTrigger");
 //}
-
-void UDMSSequenceStep_FT::Progress_FT(UDMSSequenceStep* InstancedStep)
-{
-	BroadcastProgress(InstancedStep,FName(NAME_None));
-	//auto GS = UDMSCoreFunctionLibrary::GetDMSGameState(InstancedStep); check(GS);
-	//auto SM = GS->GetSequenceManager(); check(SM);
-	//auto EH = GS->GetEffectHandler(); check(EH);
-	//auto SelM = GS->GetSelectorManager(); check(SelM);
-
-	//TArray<UDMSSelectorRequestForm> DecisionForms;
-
-	//GS->PlayerArray()
-
-	//for ( auto& DD : DecisionDefinitions )
-	//	DecisionForms.Append(DD.CreateRequestForm(InstancedStep->OwnerSequence));
-
-	//if ( WidgetOwner == nullptr || !WidgetOwner->SetupWidgetQueue(InstancedStep->OwnerSequence, SelM->RequestCreateSelectors(DecisionForms))) {
-	//	DMS_LOG_SIMPLE(TEXT("==== %s : EI Data Selection Canceled  ===="), *InstancedStep->OwnerSequence->GetName());
-
-	//	InstancedStep->ProgressEnd(false); return;
-	//}
-
-	//RunWidgetQueue(InstancedStep, WidgetOwner, [=](ADMSSequence* pSequence) {
-	//	// Run sequence ( Notifying steps and apply )
-	//	DMS_LOG_SIMPLE(TEXT("==== %s : EI Data Selection Completed  ===="), *pSequence->GetName());
-	//	InstancedStep->ProgressEnd(); return;
-	//	});
-}
+//
+////TScriptInterface<IDMSEffectorInterface> UDMSSelector_FT::GetTriggerableEffectors()
+////{
+////	return TScriptInterface<IDMSEffectorInterface>();
+////
+////	// additional condition check with owner step and find ft-able effectors.
+////
+////}
+//
+//void UDMSSequenceStep_FT::Progress_FT(UDMSSequenceStep* InstancedStep)
+//{
+//	BroadcastProgress(InstancedStep,FName(NAME_None));
+//	//auto GS = UDMSCoreFunctionLibrary::GetDMSGameState(InstancedStep); check(GS);
+//	//auto SM = GS->GetSequenceManager(); check(SM);
+//	//auto EH = GS->GetEffectHandler(); check(EH);
+//	//auto SelM = GS->GetSelectorManager(); check(SelM);
+//
+//	//TArray<UDMSSelectorRequestForm> DecisionForms;
+//
+//	//GS->PlayerArray()
+//
+//	//for ( auto& DD : DecisionDefinitions )
+//	//	DecisionForms.Append(DD.CreateRequestForm(InstancedStep->OwnerSequence));
+//
+//	//if ( WidgetOwner == nullptr || !WidgetOwner->SetupWidgetQueue(InstancedStep->OwnerSequence, SelM->RequestCreateSelectors(DecisionForms))) {
+//	//	DMS_LOG_SIMPLE(TEXT("==== %s : EI Data Selection Canceled  ===="), *InstancedStep->OwnerSequence->GetName());
+//
+//	//	InstancedStep->ProgressEnd(false); return;
+//	//}
+//
+//	//RunWidgetQueue(InstancedStep, WidgetOwner, [=](ADMSSequence* pSequence) {
+//	//	// Run sequence ( Notifying steps and apply )
+//	//	DMS_LOG_SIMPLE(TEXT("==== %s : EI Data Selection Completed  ===="), *pSequence->GetName());
+//	//	InstancedStep->ProgressEnd(); return;
+//	//	});
+//}
--- Source/ProjectDMSGame/EffectSet/Step/DMSSequenceStep_FT.h
+++ Source/ProjectDMSGame/EffectSet/Step/DMSSequenceStep_FT.h
@@ -6,27 +6,27 @@
 #include "Selector/DMSConfirmWidgetBase.h"
 #include "Selector/DMSSelectorManager.h"
 #include "Sequence/DMSSequenceStep.h"
-#include "DMSSequenceStep_FT.generated.h"
-
-class UDMSSelectorHandle;
-class UDMSSelectorRequestGenerator;
-
-/**
- * Skill test is for one tester : one test target 
- */
-UCLASS()
-class PROJECTDMSGAME_API UDMSSequenceStep_FT : public UDMSSequenceStepDefinition
-{
-	GENERATED_BODY()
-
-protected: 
-
-public: 
-	UDMSSequenceStep_FT(){}
-
-	UFUNCTION()
-	void Progress_FT(UDMSSequenceStep* InstancedStep);
-
-	virtual FGameplayTag GetPureStepTag_Implementation() const;
-};
+//#include "DMSSequenceStep_FT.generated.h"
+//
+//class UDMSSelectorHandle;
+//class UDMSSelectorRequestGenerator;
+//
+///**
+// * Skill test is for one tester : one test target 
+// */
+//UCLASS()
+//class PROJECTDMSGAME_API UDMSSequenceStep_FT : public UDMSSequenceStepDefinition
+//{
+//	GENERATED_BODY()
+//
+//protected: 
+//
+//public: 
+//	UDMSSequenceStep_FT(){}
+//
+//	UFUNCTION()
+//	void Progress_FT(UDMSSequenceStep* InstancedStep);
+//
+//	virtual FGameplayTag GetPureStepTag_Implementation() const;
+//};
 
